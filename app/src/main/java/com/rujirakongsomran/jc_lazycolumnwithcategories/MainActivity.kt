415a4cab3c0415e957ebf223c04932d472fa54cc
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-10-01 13:03:04 +0700

    Create fun CategoryHeader and CategoryItem

--- app/src/main/java/com/rujirakongsomran/jc_lazycolumnwithcategories/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_lazycolumnwithcategories/MainActivity.kt
@@ -3,10 +3,18 @@ package com.rujirakongsomran.jc_lazycolumnwithcategories
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rujirakongsomran.jc_lazycolumnwithcategories.ui.theme.JC_LazyColumnWithCategoriesTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,3 +38,37 @@ data class Category(
     val name: String,
     val items: List<String>
 )
+
+@Composable
+fun CategoryHeader(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primaryContainer)
+            .padding(16.dp)
+
+    )
+
+}
+@Composable
+fun CategoryItem(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        fontSize = 14.sp,
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(16.dp)
+
+    )
+
+}
