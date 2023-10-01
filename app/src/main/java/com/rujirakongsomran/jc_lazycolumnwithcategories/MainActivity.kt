98b3e90d0a8e57a592b6174e9da81c4a80386cd1
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-10-01 13:25:19 +0700

    Finish

--- app/src/main/java/com/rujirakongsomran/jc_lazycolumnwithcategories/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_lazycolumnwithcategories/MainActivity.kt
@@ -1,18 +1,26 @@
+@file:OptIn(ExperimentalFoundationApi::class, ExperimentalFoundationApi::class,
+    ExperimentalFoundationApi::class, ExperimentalFoundationApi::class
+)
+
 package com.rujirakongsomran.jc_lazycolumnwithcategories
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rujirakongsomran.jc_lazycolumnwithcategories.ui.theme.JC_LazyColumnWithCategoriesTheme
@@ -20,6 +28,12 @@ import com.rujirakongsomran.jc_lazycolumnwithcategories.ui.theme.JC_LazyColumnWi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val namesList = names.map {
+            Category(
+                name = it.key.toString(),
+                items = it.value
+            )
+        }
         setContent {
             JC_LazyColumnWithCategoriesTheme {
                 // A surface container using the 'background' color from the theme
@@ -27,7 +41,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    CategorizedLazyColumn(
+                        categories = namesList
+                    )
                 }
             }
         }
@@ -56,6 +72,7 @@ fun CategoryHeader(
     )
 
 }
+
 @Composable
 fun CategoryItem(
     text: String,
@@ -70,5 +87,52 @@ fun CategoryItem(
             .padding(16.dp)
 
     )
+}
 
+@Composable
+fun CategorizedLazyColumn(
+    categories: List<Category>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier) {
+        categories.forEach { category ->
+            stickyHeader {
+                CategoryHeader(category.name)
+            }
+            items(category.items) { text ->
+                CategoryItem(text = text)
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CategoryHeaderPreview() {
+    JC_LazyColumnWithCategoriesTheme {
+        CategoryHeader(text = "H")
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CategoryItemPreview() {
+    JC_LazyColumnWithCategoriesTheme {
+        CategoryItem("Ari jura")
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+fun CategorizedLazyColumnPreview() {
+    val namesList = names.map {
+        Category(
+            name = it.key.toString(),
+            items = it.value
+        )
+    }
+    JC_LazyColumnWithCategoriesTheme {
+        CategorizedLazyColumn(categories = namesList)
+    }
 }
